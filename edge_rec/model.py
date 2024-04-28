64b79516dc515bdcdb322609345ef615a06b8be2
Utkarsh Priyam <up@utkarshs-mbp.wifi.local.cmu.edu>
2024-04-27 22:34:06 -0400

    update validity check assertion

--- edge_rec/model.py
+++ edge_rec/model.py
@@ -39,7 +39,8 @@ def toi(x):
 
 def assert_in(low, high):
     def _check(x: torch.Tensor):
-        assert torch.all((low <= x) & (x < high))
+        n_low, n_high = (~(low <= x)).sum(), (~(x < high)).sum()
+        assert n_low == 0 and n_high == 0, f"n_low={n_low}, n_high={n_high}"
         return x
 
     return _check
