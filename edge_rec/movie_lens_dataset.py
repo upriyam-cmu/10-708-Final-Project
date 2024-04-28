e78a4e83817e0424611379c03f9ece3957b92861
EdoardoBotta <botta.edoardo@gmail.com>
2024-04-28 15:58:16 -0400

    Preprocessing

--- edge_rec/movie_lens_dataset.py
+++ edge_rec/movie_lens_dataset.py
@@ -20,6 +20,27 @@ class RawMovieLens100K(MovieLens100K):
     def __init__(self, root, transform=None, pre_transform=None, force_reload=False):
         super(RawMovieLens100K, self).__init__(root, transform, pre_transform, force_reload)
 
+    def __init__(self, root, transform=None, pre_transform=None, force_reload=False):
+        super(RawMovieLens100K, self).__init__(root, transform, pre_transform, force_reload)
+
+    def _bucket_ages(self, df):
+        bins = [0, 18, 25, 35, 45, 50, 56, 200]
+        labels = [0, 1, 2, 3, 4, 5, 6]
+        df["age"] = pd.cut(df["age"], bins=bins, labels=labels)
+        return df
+
+    def _process_genres(self, df):
+        l = df[self.MOVIE_HEADERS[6:]].values
+        max_genres = l.sum(axis=1).max()
+        idx_list = []
+        for i in range(l.shape[0]):
+            idxs = np.where(l[i, :] == 1)[0] + 1
+            missing = max_genres - len(idxs)
+            if missing > 0:
+                idxs = np.array(list(idxs) + missing * [0])
+            idx_list.append(idxs)
+        return np.stack(idx_list)
+        
     def process(self) -> None:
         import pandas as pd
 
@@ -36,10 +57,10 @@ class RawMovieLens100K(MovieLens100K):
         )
         movie_mapping = {idx: i for i, idx in enumerate(df.index)}
 
-        x = df[self.MOVIE_HEADERS[6:]].values
+        x = self._process_genres(df)
         data['movie'].x = torch.from_numpy(x).to(torch.float)
 
-        self.df = df
+        self.df = x
 
         # Process user data:
         df = pd.read_csv(
@@ -52,7 +73,7 @@ class RawMovieLens100K(MovieLens100K):
         )
         user_mapping = {idx: i for i, idx in enumerate(df.index)}
 
-        age = df['age'].apply(str).str.get_dummies().values.argmax(axis=1)[:, None]
+        age = self._bucket_ages(df)["age"].to_numpy()[:,None]
         age = torch.from_numpy(age).to(torch.float)
 
         gender = df['gender'].str.get_dummies().values[:, 0][:, None]
