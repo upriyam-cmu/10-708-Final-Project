fix logging of nan values & non-numerical data

edge_rec/datasets/movie_lens/data_holder.py
@@ -68,24 +68,24 @@ class MovieLensDataHolder(DataHolder):
         self.top_users, self.top_movies = self._build_density_scores(self.train_edges, self.n_users, self.n_movies)
 
     @staticmethod
-    def _split_edges(edge_indices: torch.Tensor, 
-                     edge_ratings: torch.Tensor, 
-                     test_split: float, 
-                     edge_timestamps: Optional[torch.Tensor] = None, 
+    def _split_edges(edge_indices: torch.Tensor,
+                     edge_ratings: torch.Tensor,
+                     test_split: float,
+                     edge_timestamps: Optional[torch.Tensor] = None,
                      stratify_per_user: Optional[bool] = True
-    ):
+                     ):
         assert edge_timestamps is not None or stratify_per_user
         n_edges = len(edge_ratings)
 
         edge_indices, edge_ratings = edge_indices.numpy(), edge_ratings.numpy()
-        
+
         indices_to_sort = []
         if stratify_per_user:
             indices_to_sort.append(edge_indices[0])
         if edge_timestamps is not None:
             edge_timestamps = edge_timestamps.numpy()
             indices_to_sort.append(edge_timestamps)
-        
+
         sort_by = list(range(len(indices_to_sort)))
         indices_df = pd.DataFrame(np.stack(indices_to_sort).T)
         sort_indices = indices_df.sort_values(sort_by).reset_index()["index"].to_numpy()
@@ -97,7 +97,7 @@ class MovieLensDataHolder(DataHolder):
         if stratify_per_user:
             for edge_group in np.split(np.arange(n_edges), split_indices[1:]):
                 if edge_timestamps is None:
-                    np.random.shuffle(edge_group)   
+                    np.random.shuffle(edge_group)
                 n_test = int(test_split * len(edge_group))
                 train_group.append(edge_group[:-n_test])
                 test_group.append(edge_group[-n_test:])
@@ -105,7 +105,7 @@ class MovieLensDataHolder(DataHolder):
             n_test = int(test_split * n_edges)
             train_group.append(np.arange(n_edges)[:-n_test])
             test_group.append(np.arange(n_edges)[-n_test:])
-        
+
         train_group = np.concatenate(train_group)
         test_group = np.concatenate(test_group)
 

edge_rec/datasets/movie_lens/preprocessing.py
@@ -4,7 +4,8 @@ from tqdm import tqdm
 
 
 class MovieLensPreprocessingMixin:
-    def _process_genres(self, genres, one_hot=True):
+    @staticmethod
+    def _process_genres(genres, one_hot=True):
         if one_hot:
             return genres
 
@@ -19,7 +20,8 @@ class MovieLensPreprocessingMixin:
         out = np.stack(idx_list)
         return out
 
-    def _remove_low_occurrence(self, source_df, target_df, index_col):
+    @staticmethod
+    def _remove_low_occurrence(source_df, target_df, index_col):
         if isinstance(index_col, str):
             index_col = [index_col]
         out = target_df.copy()
@@ -29,29 +31,34 @@ class MovieLensPreprocessingMixin:
             out = out.merge(high_occ, on=col).drop(columns=["ratingCnt"])
         return out
 
-    def _generate_user_history(self, ratings_df, history_len=30):
+    @staticmethod
+    def _generate_user_history(ratings_df, history_len=30):
         print("Starting User History Generation...")
         orig_order = ratings_df.reset_index()["index"]
         # Sort the DataFrame by userId and timestamp
         ratings_df = ratings_df.sort_values(by=['userId', 'timestamp'])
         unique_users = ratings_df["userId"]
-    
+
         shards = np.split(unique_users.unique(), 10)
         shards_filtered_df = []
         print("Batching User Ratings Dataframe...")
         for shard in tqdm(shards):
             shard_ratings_df = ratings_df[ratings_df["userId"].isin(shard)]
-            # Merge with itself on userId, filter the rows where timestamp_x is less than timestamp_y and rating_y is greater than 3
+            # Merge with itself on userId, filter the rows where timestamp_x
+            # is less than timestamp_y and rating_y is greater than 3
             merged_df = shard_ratings_df.merge(shard_ratings_df, on='userId', suffixes=('_x', '_y'))
-            shard_filtered_df = merged_df[(merged_df['timestamp_x'] > merged_df['timestamp_y']) & (merged_df['rating_y'] > 3)]
+            shard_filtered_df = merged_df[
+                (merged_df['timestamp_x'] > merged_df['timestamp_y']) & (merged_df['rating_y'] > 3)]
             shards_filtered_df.append(shard_filtered_df)
         filtered_df = pd.concat(shards_filtered_df)
 
         # Group by userId and timestamp_x, aggregate the movieId values as a comma-separated string
-        history_df = filtered_df.groupby(['userId', 'timestamp_x'])['movieId_y'].apply(lambda x: np.unique(list(x)[:history_len])).reset_index()
+        history_df = filtered_df.groupby(['userId', 'timestamp_x'])['movieId_y'].apply(
+            lambda x: np.unique(list(x)[:history_len])).reset_index()
 
         # Rename columns and merge with the original DataFrame
-        result_df = ratings_df.merge(history_df, left_on=['userId', 'timestamp'], right_on=['userId', 'timestamp_x'], how='left')
+        result_df = ratings_df.merge(history_df, left_on=['userId', 'timestamp'], right_on=['userId', 'timestamp_x'],
+                                     how='left')
         result_df = result_df.rename(columns={'movieId_y': 'history'}).drop(columns=['timestamp_x'])
 
         # Make sure we are preserving the original ordering

edge_rec/utils.py
@@ -142,18 +142,23 @@ class DataLogger:
         for k, v in values.items():
             # get dict to update based on value type
             if isinstance(v, Number):
-                dict_ = numerical_stats
+                is_numerical = True
                 for prefix in self._ignore_numeric:
                     if k.startswith(prefix):
-                        dict_ = misc_stats
+                        is_numerical = False
                         break
             else:
-                dict_ = misc_stats
+                is_numerical = False
 
             # update dict
-            if k not in dict_:
-                dict_[k] = []
-            dict_[k].append(v)
+            if is_numerical:
+                if k not in numerical_stats:
+                    numerical_stats[k] = []
+                elif np.isnan(numerical_stats[k][-1]):
+                    numerical_stats[k].pop()  # replace nan value instead of simply appending
+                numerical_stats[k].append(v)
+            else:
+                misc_stats[k] = v
 
         # log that keys were updated
         was_updated.update(values.keys())
@@ -185,6 +190,7 @@ class DataLogger:
 
             # bookkeeping
             was_updated.clear()
+            misc_stats.clear()
             self._step += 1
 
 